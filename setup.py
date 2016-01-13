d68f662ec6a997b74ea4d241708728da03a62c95
kyleknap <kyleknap@amazon.com>
2016-01-13 09:36:56 -0800

    Add the setup files

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+
+requires = []
+
+
+if sys.version_info[0] == 2:
+    # concurrent.futures is only in python3, so for
+    # python2 we need to install the backport.
+    requires.append('futures>=2.2.0,<4.0.0')
+
+
+def get_version():
+    init = open(os.path.join(ROOT, 's3transfer', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+setup(
+    name='s3transfer',
+    version=get_version(),
+    description='An Amazon S3 Transfer Manager',
+    long_description=open('README.rst').read(),
+    author='Amazon Web Services',
+    url='https://github.com/boto/s3transfer',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=requires,
+    extras_require={
+        ':python_version=="2.6" or python_version=="2.7"': [
+            'futures>=2.2.0,<4.0.0']
+    },
+    license="Apache License 2.0",
+    classifiers=(
+        'Development Status :: 1 - Planning'
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
+)

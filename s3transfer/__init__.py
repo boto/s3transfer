d7f9202c1360a9798ee711d3f06df8aebc5e8dbf
kyleknap <kyleknap@amazon.com>
2016-08-09 12:33:21 -0700

    Add null handler to s3transfer

--- s3transfer/__init__.py
+++ s3transfer/__init__.py
@@ -145,7 +145,15 @@ from s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 __author__ = 'Amazon Web Services'
 __version__ = '0.1.1'
 
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
 logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
 queue = six.moves.queue
 
 MB = 1024 * 1024

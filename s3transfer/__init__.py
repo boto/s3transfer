852a58c0dab209c4b018e515394f0dfd469efe9b
AWS <aws@amazon.com>
2016-12-15 15:54:07 -0800

    Bumping version to 0.1.10

--- s3transfer/__init__.py
+++ s3transfer/__init__.py
@@ -143,7 +143,7 @@ from s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 
 __author__ = 'Amazon Web Services'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 
 class NullHandler(logging.Handler):

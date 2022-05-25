f3c233cc27924a6aff56bc13c74a2dabc6e7c2bb
Nate Prewitt <nate.prewitt@gmail.com>
2022-05-25 09:56:41 -0600

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'crt': 'botocore[crt]>=1.20.29,<2.0a.0',
     },
     license="Apache License 2.0",
-    python_requires=">= 3.6",
+    python_requires=">= 3.7",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -41,7 +41,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',

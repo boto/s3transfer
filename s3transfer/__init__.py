528a8b8e880dddaff864d304706b071ce27bdd9b
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2025-04-17 18:05:30 +0000

    Bumping version to 0.11.5

--- s3transfer/__init__.py
+++ s3transfer/__init__.py
@@ -145,7 +145,7 @@ import s3transfer.compat
 from s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 __author__ = 'Amazon Web Services'
-__version__ = '0.11.4'
+__version__ = '0.11.5'
 
 
 class NullHandler(logging.Handler):

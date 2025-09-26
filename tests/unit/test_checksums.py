738d3ac8f14df7119fcdf8d163878575a68447e1
Steve Yoo <hssyoo@amazon.com>
2025-09-26 11:29:35 -0400

    Update tests

--- tests/unit/test_checksums.py
+++ tests/unit/test_checksums.py
@@ -10,60 +10,51 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import random
 from binascii import crc32
-
-import pytest
 
 from s3transfer.checksums import combine_crc32
 
 
 class TestCombineCrc32:
-    def _calculate_crc32_serial(self, data):
-        return crc32(data) & 0xFFFFFFFF
+    def test_combine(self):
+        for _ in range(100):
+            data1 = random.randbytes(32)
+            crc1 = crc32(data1)
+            data2 = random.randbytes(32)
+            crc2 = crc32(data2)
+            serial = crc32(data1 + data2)
+            combined = combine_crc32(crc1, crc2, len(data2))
+            assert serial == combined
 
-    def _calculate_crc32_combined(self, data1, data2):
-        crc1 = crc32(data1)
-        crc2 = crc32(data2)
-        return combine_crc32(crc1, crc2, len(data2))
+    def test_combine_no_update(self):
+        data = random.randbytes(32)
+        init = random.randint(1, 0x80000000)
+        serial = crc32(data, init)
+        combined = combine_crc32(init, crc32(data), len(data))
+        assert serial == combined
 
-    @pytest.mark.parametrize(
-        "data1,data2",
-        [
-            (b"foo", b"bar"),
-            (b"", b""),
-            (b"", b"foobar"),
-            (b"foobar", b""),
-            (b"foobar", b"foobar"),
-            (b"foo" * 10000, b"bar" * 10000),
-            (b"\x00", b"\x00"),
-        ],
-    )
-    def test_combine_crc32(self, data1, data2):
-        serial_crc = self._calculate_crc32_serial(data1 + data2)
-        combined_crc = self._calculate_crc32_combined(data1, data2)
-        assert combined_crc == serial_crc
-
-    def test_combine_crc32_many_parts(self):
+    def test_combine_many_parts(self):
         parts = [f"Part{i}".encode() for i in range(1000)]
 
-        serial_crc = self._calculate_crc32_serial(b"".join(parts))
-        combined_crc = self._calculate_crc32_serial(parts[0])
+        serial_crc = crc32(b"".join(parts))
+        combined_crc = crc32(parts[0])
         for i in range(1, len(parts)):
-            part_crc = self._calculate_crc32_serial(parts[i])
+            part_crc = crc32(parts[i])
             combined_crc = combine_crc32(combined_crc, part_crc, len(parts[i]))
 
         assert combined_crc == serial_crc
 
-    def test_combine_crc32_associative_property(self):
+    def test_combine_associative_property(self):
         data_a = b"foo"
         data_b = b"bar"
         data_c = b"baz"
 
-        serial_crc = self._calculate_crc32_serial(data_a + data_b + data_c)
+        serial_crc = crc32(data_a + data_b + data_c)
 
-        crc_a = self._calculate_crc32_serial(data_a)
-        crc_b = self._calculate_crc32_serial(data_b)
-        crc_c = self._calculate_crc32_serial(data_c)
+        crc_a = crc32(data_a)
+        crc_b = crc32(data_b)
+        crc_c = crc32(data_c)
 
         # (a+b) + c
         crc_ab = combine_crc32(crc_a, crc_b, len(data_b))
